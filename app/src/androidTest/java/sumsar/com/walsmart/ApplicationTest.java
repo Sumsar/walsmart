1edd219a3aa9e16ef4d199bae67924c790c627e8
Rasmus S. Gohs <Rasmus.Gohs@jayway.com>
2015-09-09 15:42:34 +0200

    Initial commit

---
+++ app/src/androidTest/java/sumsar/com/walsmart/ApplicationTest.java
@@ -0,0 +1,13 @@
+package sumsar.com.walsmart;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
