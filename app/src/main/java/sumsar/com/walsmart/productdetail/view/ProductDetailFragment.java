ffd4c01b55c29cac56bc29cb4112bc5acfda098e
Rasmus S. Gohs <Rasmus.Gohs@jayway.com>
2015-09-23 11:01:02 +0200

    Scroll to top on select product (Tablet)

--- app/src/main/java/sumsar/com/walsmart/productdetail/view/ProductDetailFragment.java
+++ app/src/main/java/sumsar/com/walsmart/productdetail/view/ProductDetailFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import sumsar.com.walsmart.BR;
 import sumsar.com.walsmart.R;
@@ -19,16 +20,27 @@ import sumsar.com.walsmart.util.MyLog;
 /**
  * Created by rasmusgohs on 09/09/15.
  */
-public class ProductDetailFragment extends Fragment{
+public class ProductDetailFragment extends Fragment {
 
 
     private ViewDataBinding mDataBinding;
+    /**
+     * Is only present in the tablet layout. Thus it can be null
+     */
+    @Nullable
+    private ScrollView      mScrollView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mDataBinding = DataBindingUtil.inflate(inflater, getLayout(getContext()), container, false);
         return mDataBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mScrollView = (ScrollView) view.findViewById(R.id.product_detail_scroll_view);
     }
 
     @LayoutRes
@@ -42,6 +54,12 @@ public class ProductDetailFragment extends Fragment{
             MyLog.e(ProductDetailFragment.class.getSimpleName(), "Product is null");
             return;
         }
+
+
+        if (mScrollView != null) {
+            mScrollView.fullScroll(ScrollView.FOCUS_UP);
+        }
+
         mDataBinding.setVariable(BR.product, product);
         mDataBinding.executePendingBindings();
 
