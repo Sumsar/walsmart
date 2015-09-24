a4d84ad7a1e956c340d080988701f2a26c4d815a
Rasmus S. Gohs <Rasmus.Gohs@jayway.com>
2015-09-24 12:12:22 +0200

    Added espresso test

--- app/src/androidTest/java/sumsar/com/walsmart/ApplicationTest.java
+++ app/src/androidTest/java/sumsar/com/walsmart/ApplicationTest.java
@@ -1,13 +1,44 @@
 package sumsar.com.walsmart;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+import sumsar.com.walsmart.productlist.view.ProductListActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class ApplicationTest extends ActivityInstrumentationTestCase2<ProductListActivity> {
+
+
     public ApplicationTest() {
-        super(Application.class);
+        super(ProductListActivity.class);
     }
+
+    public ApplicationTest(Class<ProductListActivity> activityClass) {
+        super(activityClass);
+    }
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    @MediumTest
+    public void testSelectProduct() throws InterruptedException {
+        Thread.sleep(4000);
+
+        onView(withId(R.id.product_list_recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.product_detail_name)).check(matches(withText("Ellerton TV Console")));
+
+
+    }
+
 }
